46f7e7b39ad06e32a4963899ba1cbe3e7b203d6d
Serhii Zhuravel <serhii.zhuravel@absio.com>
2019-04-16 15:17:20 +0300

    Implemented layout and example component

---
+++ components/NewsCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card height="300px">
+    <v-img :src="article.urlToImage" :alt="article.title" contain></v-img>
+    <v-card-title>
+      <v-card-actions>{{ article.title }}</v-card-actions>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default() {
+        return {
+          urlToImage: '#',
+          title: ''
+        }
+      }
+    }
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,8 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv'
   ],
   /*
    ** Axios module configuration
