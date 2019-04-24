e830b87ad4327810234e9c7796b4fc6eef0a4b78
Serhii Zhuravel <serhii.zhuravel@absio.com>
2019-04-24 12:04:50 +0300

    Added mobile menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,31 @@
 <template>
   <v-app dark>
     <v-toolbar>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title><nuxt-link to="/">Home</nuxt-link> </v-toolbar-title>
+      <v-menu class="hidden-md-and-up">
+        <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title
+                ><nuxt-link to="/inspire">Inspire</nuxt-link></v-list-tile-title
+              >
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title @click="increment">Like</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-toolbar-title
+        ><nuxt-link to="/">Home - {{ counter }} Likes</nuxt-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat><nuxt-link to="/inspire">Inspiration</nuxt-link></v-btn>
         <v-btn flat to="/inspire">Link Two</v-btn>
         <v-btn flat @click="increment">Increment</v-btn>
-        Counter:{{ counter }}
       </v-toolbar-items>
     </v-toolbar>
 
