f6c4dcd83ca2b8a0360aa73a13c5583c0aaa0ece
Serhii Zhuravel <serhii.zhuravel@absio.com>
2019-04-24 11:34:11 +0300

    Updates

--- sw.js
+++ sw.js
@@ -2,32 +2,28 @@ importScripts('/vue-pwa-demo/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/vue-pwa-demo/_nuxt/06aecc730d713b397b16.js",
-    "revision": "a8ab40ef0797110262c8f119385aeba0"
+    "url": "/vue-pwa-demo/_nuxt/0bcf40b35aa7457b5cad.js",
+    "revision": "e87a84dde885964ca1b4c727edb5abbe"
   },
   {
-    "url": "/vue-pwa-demo/_nuxt/153b7f240c676a5235fc.js",
-    "revision": "373d73ac1b5c88f66c6fc45336a1612c"
+    "url": "/vue-pwa-demo/_nuxt/0cd03e9a71cc436a37aa.js",
+    "revision": "71d2f9bac747ee55d1dd8474e0e6e5e4"
   },
   {
-    "url": "/vue-pwa-demo/_nuxt/246a92383838d8a668e0.js",
-    "revision": "23c5b60a617c9d1dd358d5ba1bd3641e"
+    "url": "/vue-pwa-demo/_nuxt/25850eaa3bb6f24ea0ce.js",
+    "revision": "ee2fce4d7af856001707e6c92d6d4c5b"
   },
   {
-    "url": "/vue-pwa-demo/_nuxt/7ca5581e0b5c804fda7a.js",
-    "revision": "6e00d2cdc33b1e6eb9c5fcbdea373039"
+    "url": "/vue-pwa-demo/_nuxt/82c0579cf036ca1c0d2e.js",
+    "revision": "654e0f0df07153c947eb56720477aef4"
   },
   {
-    "url": "/vue-pwa-demo/_nuxt/7cfa1cd6425efd9bab65.js",
-    "revision": "465280e93f41a7646ec185b41d88e954"
+    "url": "/vue-pwa-demo/_nuxt/950261bc5d4b48878924.js",
+    "revision": "c67301f281c22b00509217a7dd3dd6c4"
   },
   {
-    "url": "/vue-pwa-demo/_nuxt/8c07da849f31ca374686.js",
-    "revision": "a80c4667230252dfce9040c1926c1754"
-  },
-  {
-    "url": "/vue-pwa-demo/_nuxt/99086f8ba4eadb436d24.js",
-    "revision": "0daf376f01129774f1aaa58b966c6fe8"
+    "url": "/vue-pwa-demo/_nuxt/d2ab45777f41e1f902ea.js",
+    "revision": "7caa0502e0ed239767c8dc5040bdef74"
   }
 ], {
   "cacheId": "nuxt_demo_app",
